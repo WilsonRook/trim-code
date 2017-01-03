b118fbaa11b49e49b277798c909d19d7be4184d8
Wilson Rook <Wilson.Rook@gmail.com>
2017-01-03 07:49:31 -0500

    updated index.php

--- index.php
+++ index.php
@@ -1,4 +1,43 @@
-<?
+<?php 
+//include("inc/connection.php");
+//include("inc/formjs.php");
+$pageTitle = "Candidate Quiz"; 
 
-echo "This is my first PHP file!";
+include("views/header.php"); 
+?>
 
+<div align="center" class="">
+	<h1>Welcome to the Trim Agency Apprentice Quiz</h1>
+</div>
+
+
+<?php 
+		// Check database for $name match
+		// $sth = $pdo->prepare(
+		// 	"SELECT * 
+		// 	FROM users 
+		// 	WHERE name = ?"
+		// );
+		// $sth->execute([$name]);
+		// $result = $sth->fetch();
+		//$result = $sth->fetchAll();
+
+//******some SQL code for sample
+//		var_export($result);
+//	echo "<br />\n"."Name not in database"; /////******   make Registration form.
+?>
+
+<div class="section page">      <!-- class="section page">   -->
+	<div>  <!-- // class ="wrapper">  -->
+		<h3 align="center">Please take following quiz.</h3>	
+		
+		<?php 
+			//****** Form included here located in views folder **********
+			include("views/form.php"); 
+		?>
+
+	</div>
+</div>
+
+
+<?php include("views/footer.php");?>
