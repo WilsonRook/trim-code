5d0875982b1cb610cab0066ea424f4ec34d38eb5
Wilson Rook <Wilson.Rook@gmail.com>
2017-01-04 06:05:31 -0500

    adding test file

---
+++ views/testthankyou.php
@@ -0,0 +1,12 @@
+<?php 
+    //include("inc/connect.php");
+    $pageTitle = "Test Thank You!"; 
+
+    include("header.php"); 
+?>
+
+<div class="section page">      <!-- class="section page">   -->
+    <div align="center" class="">  <!-- // class ="wrapper">  -->
+        <h1>Test  TEST    Thank you! Your request is being processed.</h1>
+        <h3>Below is the contents of the database.</h3>
+    </div>
