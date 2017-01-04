9cc4abd3d35a7aac64fa2c493a72a297e2da3e34
Wilson Rook <Wilson.Rook@gmail.com>
2017-01-04 07:19:46 -0500

    playing with connect.php to get PDO settings right

--- inc/connect.php
+++ inc/connect.php
@@ -1,21 +1,41 @@
 <?php
 
-$dbopts = parse_url(getenv('DATABASE_URL'));
-$app->register(new Herrera\Pdo\PdoServiceProvider(),
-               array(
-                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["ec2-23-21-184-181.compute-1.amazonaws.com
-"] . ';port=' . $dbopts["5432"],
-                   'pdo.username' => $dbopts["vboszvnnbteipi"],
-                   'pdo.password' => $dbopts["9b76f6ced090dfad9ff9be358c3f1a28d0c18eb3410f5ea31c550d04d0364903"]
-               )
-);
-
-//original code from heroku will test if needing to put in  credentials.
 // $dbopts = parse_url(getenv('DATABASE_URL'));
 // $app->register(new Herrera\Pdo\PdoServiceProvider(),
 //                array(
-//                    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
-//                    'pdo.username' => $dbopts["user"],
-//                    'pdo.password' => $dbopts["pass"]
+//                    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["ec2-23-21-184-181.compute-1.amazonaws.com
+// "] . ';port=' . $dbopts["5432"],
+//                    'pdo.username' => $dbopts["vboszvnnbteipi"],
+//                    'pdo.password' => $dbopts["9b76f6ced090dfad9ff9be358c3f1a28d0c18eb3410f5ea31c550d04d0364903"]
 //                )
 // );
+
+//original code from heroku will test if needing to put in  credentials.
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+               array(
+                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+                   'pdo.username' => $dbopts["user"],
+                   'pdo.password' => $dbopts["pass"]
+               )
+);
+
+
+
+// some code that worked in another project
+
+$host = $app['pdo.dsn.host'];
+$dbname = $app['pdo.dsn.pgsql:dbname'];
+$user = $app['pdo.username'];
+$pass = $app['pdo.password'];
+$charset = 'utf8';
+
+//$dsn = 'mysql:dbname=inteltre_medialibrary;host=localhost';
+
+$dsn = "pgsql:host=$host;dbname=$dbname;charset=$charset";
+$opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false
+];
+$pdo = new PDO($dsn, $user, $pass, $opt);
